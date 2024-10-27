47d15c352f5ffed83da07171aefce50fc9c3f3ee
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-27 17:15:40 +0100

    Encerrando o Basico de JavaScript

--- script.js
+++ script.js
@@ -1,95 +1,34 @@
-/* var area = document.getElementById('area')
-//entrar
-function entrar(){
-   var nome = prompt('Digite o seu nome')
+/* */
+var peso;
+var altura;
+var resultado;
+var imc;
 
-   if(nome === '' || nome === null){
-    alert('deu algo errado!')
-    area.innerHTML="Clique no botao para acessar..."
-   }else{
-    area.innerHTML="Bem vindo" +" "+ nome + " ";
+function calcular(event){
+    event.preventDefault()
 
-    let botaoSair = document.createElement("button");
-    botaoSair.innerText = "Sair da Conta";
-    botaoSair.onclick   =   sair;
-    area.appendChild(botaoSair)
-   }
+    peso=document.getElementById('peso').value ;
+    altura= document.getElementById('altura').value;
 
-}
-//sair
-function sair(){
-    alert('Até mais !')
-    area.innerHTML='Voce Saiu!'
-}
-//media
-function mediaAluno(nota1,nota2){
-    var media = (nota1 + nota2) / 2;
+    imc = peso/(altura * altura) 
 
-    if(media >=7){
-        console.log('Aluno Aprovado com  a média: ' + media)
-    }else if(media < 7){
-        console.log('Aluno Reprovado com a média: '+ media)
+    console.log("peso : " + peso + ' altura : ' +altura + ' IMC: ' + imc)
+
+    resultado = document.getElementById('resultado')
+
+    if(imc < 17) {
+        resultado.innerHTML= '<br/> Seu resultado IMC foi de : ' + imc.toFixed(2) + '<br/> muitoABAIXO DO PESO'
+    } else if (imc > 17 && imc <= 18.49) {
+        resultado.innerHTML= '<br/> Seu resultado IMC foi de : ' + imc.toFixed(2) + '<br/> ABAIXO DO PESO'
+    } else if(imc >=18.5 && imc < 24.99){
+        resultado.innerHTML= '<br/> Seu resultado IMC foi de : ' + imc.toFixed(2) + '<br/> PESO IDEAL'
+    }else if(imc > 25 && imc <= 29.99){
+        resultado.innerHTML= '<br/> Seu resultado IMC foi de : ' + imc.toFixed(2) + '<br/> ACIMA DO PESO'
+    }else if(imc >= 30){
+        resultado.innerHTML= '<br/> Seu resultado IMC foi de : ' + imc.toFixed(2) + ' OBESIDADE'
     }
-}
-//aluno com o curso
-function aluno(aluno,curso){
-    var mensagem = 'Seja bem vindo ' + aluno + ' ao curso de ' + curso;
-    console.log(mensagem)
-} */
-
-    //                  WHILE  = enquanto
-
-/* var x = 5;
-while(x < 10){
-    document.write('<br> O valor do X : ' + x);
-
-    //Aumentar o valor do X (incrementar X)
-    x++;
-} */
-
-//                  FOR = PARA
-
-/* var valor = 30;
-
-for(a=0;a<=valor;a++){
-document.write('<br>Valor do A : ' + a);
-}
-
- */
-
-//                      SWITCH
-function pedir(){
-    var valor = prompt('Digite um valor Tiago || Joana')
-   // console.log(typeof  Number(valor))
-
-    switch(valor){
-        case 'Tiago':
-         alert('Ola Tiago');
-            break;
-        case 'Joana':
-            alert('Ola Joana')
-            break;
-        default:
-            alert('Algum NOME da lista')    
-    }
-
-
-
-  /*  switch(Number(valor)){
-    case 1:
-        alert('Voce escolheu o 1 : Sumo')
-        break;
-    case 2:
-        alert('Voce escolheu o 2 : Agua Gelada')
-        break;
-    case 3:
-        alert('Voce escolheu o 3 : Gelado');
-            break;
-    case 4:
-        alert('Voce chamou o Empregado')
-        break;
-    default:
-        alert('Escolha entre 1 e 4')
-   } */
+//          resetar valores apos submit
+    document.getElementById('peso').value=''; 
+    document.getElementById('altura').value='';
 
 }

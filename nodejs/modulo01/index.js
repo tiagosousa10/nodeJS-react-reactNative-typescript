2424a1b5a9ec3dc69d063e914e4aa66ca2b8a40e
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-29 21:15:27 +0100

    Query e Route Params

--- nodejs/modulo01/index.js
+++ nodejs/modulo01/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 
 const server = express()
 
-// localhost:3000/curso
-server.get('/curso' , (req,res) => {
-  return res.json({curso:'NodeJS'})
+//Query Params = ?nome=NodeJS
+//Route Params = /curso/2
+//Request Body = {nome: 'Nodejs', tipo: 'Backend'}
+
+// localhost:3000/curso/2
+server.get('/curso/:id' , (req,res) => {
+  const id = req.params.id;
+
+  return res.json({curso:`Curso:  ${id}`})
 })
 
 server.listen(3000)

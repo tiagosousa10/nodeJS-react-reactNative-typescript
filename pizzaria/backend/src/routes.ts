4ae1ecbdc2a1e5f8fc2daa2f0cb952e7e8fd97b3
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-30 23:34:03 +0100

    controllers & services

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -1,11 +1,9 @@
-import {Router,Request,Response} from 'express'
-
+import {Router} from 'express'
+import {CreateUserController} from './controllers/user/CreateUserController'
 const router = Router()
 
-router.get('/teste', (req:Request ,res:Response) => {
-    return res.json({nome:'Tiago'})
-})
-
+// --- ROTAS USER ---
+router.post('/users', new CreateUserController().handle)
 
 
 export {router};

e326c95c52d137aa48bde4050b2d62e947e97a28
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-31 23:52:41 +0100

    Adicionar Items na Order

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -14,6 +14,7 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 
 import {CreateOrderController} from './controllers/order/CreateOrderController'
 import {RemoveOrderController} from './controllers/order/RemoveOrderController'
+import {AddItemController} from './controllers/order/AddItemController'
 
 //                                                  MIDDLEWARES
 import {isAuthenticated} from './middlewares/isAuthenticated'
@@ -39,5 +40,6 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 //--- ROTAS ORDER ---
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export {router};

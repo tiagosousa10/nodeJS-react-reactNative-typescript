c68c791ae17e4fb2997bbd74275b7a2f82c9cf6b
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-31 23:34:53 +0100

    Adicionar / Remover Pedidos (orders)

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -12,6 +12,9 @@ import {ListCategoryController} from './controllers/category/ListCategoryControl
 import {CreateProductController} from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
+import {CreateOrderController} from './controllers/order/CreateOrderController'
+import {RemoveOrderController} from './controllers/order/RemoveOrderController'
+
 //                                                  MIDDLEWARES
 import {isAuthenticated} from './middlewares/isAuthenticated'
 //                                                  MULTER 
@@ -32,5 +35,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle )
 //--- ROTAS PRODUCT ---
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+//--- ROTAS ORDER ---
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export {router};

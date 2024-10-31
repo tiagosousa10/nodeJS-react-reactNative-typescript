f97bc9e1144b0db696026edae4fe680bab01b48b
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-31 20:14:43 +0100

    categorias + produtos

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -3,6 +3,11 @@ import {Router} from 'express'
 import {CreateUserController} from './controllers/user/CreateUserController'
 import {AuthUserController} from './controllers/user/AuthUserController'
 import {DetailUserController} from './controllers/user/DetailUserController'
+
+import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+import {ListCategoryController} from './controllers/category/ListCategoryController'
+
+import {CreateProductController} from './controllers/product/CreateProductController'
 //                                                  MIDDLEWARES
 import {isAuthenticated} from './middlewares/isAuthenticated'
 
@@ -13,5 +18,11 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me',isAuthenticated, new DetailUserController().handle )
 
+// --- ROTAS CATEGORY ---
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+//--- ROTAS PRODUCT ---
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export {router};

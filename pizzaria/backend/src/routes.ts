66b5c68d8247f89b7b3c7831c5d71f7067877008
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-01 01:12:42 +0100

    enviar, verificar e concluir pedidos + backend realizado

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -16,6 +16,10 @@ import {CreateOrderController} from './controllers/order/CreateOrderController'
 import {RemoveOrderController} from './controllers/order/RemoveOrderController'
 import {AddItemController} from './controllers/order/AddItemController'
 import {RemoveItemController} from './controllers/order/RemoveItemController'
+import {SendOrderController} from './controllers/order/SendOrderController'
+import {ListOrdersController} from './controllers/order/ListOrdersController'
+import {DetailOrderController} from './controllers/order/DetailOrderController'
+import {FinishOrderController} from './controllers/order/FinishOrderController'
 
 //                                                  MIDDLEWARES
 import {isAuthenticated} from './middlewares/isAuthenticated'
@@ -43,5 +47,10 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders/', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
+
 
 export {router};

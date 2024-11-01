3709605e0d414f8af3d85b123802c3ea9e435c2e
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-01 12:53:49 +0100

    nextJS Initial commit

---
+++ meuprojeto/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function Contatos(){
+    return(
+        <div>
+            <h1>Pagina de Contatos</h1>
+            <span>(xx) --- --- ---</span>
+        </div>
+    )
+}
---
+++ meuprojeto/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home(){
+  return(
+    <main>
+      <h1>Pagina Home</h1>
+      <span>Seja bem vindo!</span>
+      <br/>
+
+    </main>
+  )
+}

58c4bb84c7faa6f49a167d471e44ee1ff6e7dd08
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-01 17:04:02 +0100

    login + signup + scss

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,41 +1,50 @@
-import { symlink } from 'fs'
-import styles from './page.module.scss'
+import Image from "next/image"
+import Link from "next/link"
+import styles from '../page.module.scss'
 import logoImg from '/public/logo.svg'
-import Image from 'next/image'
-import Link from 'next/link'
 
-export default function Page(){
-  return(
-    <div className={styles.containerCenter} >
+export default function Signup(){
+    return(
+        <>
+        <div className={styles.containerCenter} >
       <Image src={logoImg} alt="Logo da pizzaria"     />
 
       <section className={styles.login}>
+        <h1>Criar a sua Conta</h1>
         <form action="">
+         <input 
+          type="text" 
+          required 
+          name='name' 
+          placeholder='Digite o seu nome...' 
+          className={styles.input}
+        />
          <input 
           type="email" 
           required 
           name='email' 
           placeholder='Digite o seu e-mail...' 
           className={styles.input}
-    />
+        />
 
-<input 
+        <input 
           type="password" 
           required 
           name='password' 
           placeholder='**********' 
           className={styles.input}
-    />
+        />
 
     <button type='submit' className={styles.button}>
-      Acessar
+      Cadastrar
     </button>
     </form>
 
-    <Link href='/signup' className={styles.text}>
-    Não possui uma conta? Cadastre-se
+    <Link href='/' className={styles.text}>
+        Já possui uma conta? Faça o Login.
     </Link>
       </section>
     </div>
-  )
+        </>
+    )
 }

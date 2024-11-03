eb572f7c3e38b5a98c7ea65d32acbd86faa8b72d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-03 22:41:52 +0100

    corrigindo erros v1

--- pizzaria/frontend/src/middleware.ts
+++ pizzaria/frontend/src/middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(req:NextRequest){
 
     const {pathname} = req.nextUrl
 
-    if(pathname.startsWith("/_next" || pathname === "/")){
+    if(pathname.startsWith("/_next") || pathname === "/"){
         return NextResponse.next()
     }
 

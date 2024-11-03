e951fe7c61a52039d8b03c45bfd21ffe8ff3edc7
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-03 17:16:55 +0100

    finalizando projetoweb

--- pizzaria/frontend/src/app/dashboard/components/modal/index.tsx
+++ pizzaria/frontend/src/app/dashboard/components/modal/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss'
 import { X} from 'lucide-react'
 import {use} from 'react'
 import {OrderContext} from '@/providers/order'
+import {calculateTotalOrder} from '@/lib/helper'
 
 export function Modalorder(){
     const {onRequestClose,order, finishOrder} = use(OrderContext)
@@ -31,13 +32,15 @@ export function Modalorder(){
                         {order.map((item) => (
                                    <section className={styles.item} key={item.id}>
                                    <span>
-                                       {item.amount} - <b>{item.product.name}</b>
+                                   Qtd:{item.amount} - <b>{item.product.name}</b> - $ {parseFloat(item.product.price) * item.amount}
+
                                    </span>
                                    <span className={styles.description}>{item.product.description} </span>
                                </section>
        
                         ))}
 
+                        <h3  className={styles.total}>Valor total : $ {calculateTotalOrder(order)}</h3>
 
                         <button className={styles.buttonOrder} onClick={handleFinishOrder}>
                             Concluir Pedido

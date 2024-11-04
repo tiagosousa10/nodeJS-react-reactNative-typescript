4b5286c716525d62d937d17cbe3c67bd9fb33189
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-04 12:41:13 +0100

    primeirasAulas - reactnative

--- tarefas/App.js
+++ tarefas/App.js
@@ -1,11 +1,46 @@
-import React from 'react'
-import { View,Text} from 'react-native'
+import React,{useState} from 'react'
+import { View,Text,StyleSheet, TouchableOpacity} from 'react-native'
 
 export default function App(){
+  const [nome,setNome] = useState('Fulano')
+
+  function handleMudaNome(){
+    setNome('Sujeito Programador')
+  }
   return(
-    <View style={{flex:1,backgroundColor: '#121212', paddingTop:28 }}>
-      <Text style={{fontSize:28}}>Tiago</Text>
-      <Text> Programador</Text>
+    <View style={styles.container }>
+      <Text style={styles.title}>{nome}  </Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleMudaNome} >
+        <Text style={styles.buttonText}>Mudar nome</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#f1f1f1',
+    paddingTop:28,
+  },
+  title:{
+    fontSize:32,
+    color:"#121212",
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+  button:{
+    backgroundColor:'blue',
+    height:40,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:35,
+  },
+  buttonText:{
+    color:'#fff',
+    fontWeight:'bold',
+  }
+  
+})
